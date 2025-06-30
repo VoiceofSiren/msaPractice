b4b1882af2be9f23e2c5a869e1fe7b583cefaa2a
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2025-06-30 16:10:20 +0900

    feat: enable eureka client

--- src/main/java/com/example/test1/Test1Application.java
+++ src/main/java/com/example/test1/Test1Application.java
@@ -2,7 +2,9 @@ package com.example.test1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Test1Application {
 
